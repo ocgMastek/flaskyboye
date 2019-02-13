25c58337173466b33fe6b0c6daf77b1546d05f9f
Oliver <44974906+ocgMastek@users.noreply.github.com>
2019-02-13 11:54:02 +0000

    accessing form data server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+import jsonpickle
 
 app = Flask(__name__)
 
@@ -11,7 +12,27 @@ def hello_world():
 def return_template():
     return render_template('home.html')
 
-@app.route('/register', methods=['POST'])
+@app.route('/patient', methods=['GET'])
+def register_form():
+    if request.method == 'GET':
+        return render_template('patients.html')
+
+@app.route('/patient/register', methods=['POST'])
 def register_patient():
-    if request.method == "POST":
-        return 'POST'
+    if request.method == 'POST':
+        fullrequest = request.form
+        patient = ({"patient_id":int(request.form.get("patient_id")),
+                    "name":request.form.get("name"),
+                    "age":int(request.form.get("age")),
+                    "area":request.form.get("area")
+                    })
+        return jsonpickle.encode(patient)
+        # patientid = ({"patient_id":int(request.form.get("patient_id"))})
+
+        # patientname = ({"name":str(request.form.get("name"))})
+        
+
+    # if request.method == 'GET':
+    #     return render_template('patients.html')        
+    # elif request.method == 'POST':
+    #     return 'sent post'
