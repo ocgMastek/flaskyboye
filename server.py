f9d43ba5380bcf588347eca4cad053494bc78227
Oliver <44974906+ocgMastek@users.noreply.github.com>
2019-02-14 16:23:04 +0000

    created record interface

--- server.py
+++ server.py
@@ -6,6 +6,20 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+mysqlconnector://root:root@localhost:3306/python_projet'
 db  = SQLAlchemy(app)
 
+class patient_report(db.Model):
+    report_id=db.Column(db.Integer, primary_key=True)
+    patient_id=db.Column(db.Integer, primary_key=True)
+    report_text=db.Column('report', db.String(250))
+
+    def __init__(self,params):
+        __tablename__="patient_notes"
+        self.report_id=params["notes"]
+        self.patient_id=int(params["patient_id"])
+        pass
+
+    def __str__(self):
+        return "patient" + str(self.patient_id)+ ", reported history: " + self.report_text
+
 class Patient(db.Model):
     __tablename__="patients"
     patient_id=db.Column(db.Integer, primary_key=True)
@@ -50,6 +64,21 @@ def lab_manager():
     if request.method == 'GET':
         return render_template('lab-manager.html',patients=Patient.query.all())
         
+@app.route('/patient/report', methods=['GET'])
+def make_report():
+    if request.method == 'GET':
+        return render_template('record.html')
+
+
+@app.route('/patient/savereport', methods=['POST'])
+def save_report():
+    if request.method == "POST":
+        new_report = patient_report({"patient_id":request.form.get("patient_id"),
+                                     "notes":request.form.get("notes")})
+        print(new_report)
+    return redirect('/patient/report')
+    #return render_template('record.html')
+
 
 @app.route('/patient/register', methods=['POST'])
 def register_patient():
